7b22221806599fa264ac2ab9f831d02e9e3a9369
codewithomarm <codewithomarm@gmail.com>
2024-08-29 16:32:43 -0500

    Agregando Laboratorio2 finalizado

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Client client = new Client();
+        // Ejemplo utilizando las variables de instancia de Clase.
+        client.FirstName = "Omar";
+        client.LastName = "Montoya";
+        client.Age = 28;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFullName());
+    }
+
+    public class Client
+    {
+        // Declarando variables de instancia en clase.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            // Utilizando variables de instancia dentro de metodos de la clase.
+            return FirstName + " " + LastName;
+        }
+    }
+}
