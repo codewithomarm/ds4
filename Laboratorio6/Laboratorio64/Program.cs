9f5e6e10a59ecf7ec5b54fc71967ab14a021690e
codewithomarm <codewithomarm@gmail.com>
2024-09-16 19:18:39 -0500

    Agregando laboratorio 6

---
+++ Laboratorio6/Laboratorio64/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void checkAge(int age)
+    {
+        if (age <10)
+        {
+            throw new ArithmeticException("Acceso negado = No cumple con el criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Acceso concedido");
+        }
+    }
+    private static void Main(string[] args)
+    {
+        checkAge(15);
+    }
+}
